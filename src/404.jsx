8a66116424ff9d9b56d4138a4cfc28e8efc3b958
fkornu <fkornu@oneplan.com.gh>
2022-11-05 18:18:28 +0000

    feature: build changes

---
+++ src/404.jsx
@@ -0,0 +1,11 @@
+export default function PageNotFound(){
+  return (
+    <div className="containers">
+ 
+      <h1 className="mt-90">404</h1>
+      <h2>Oops! Page not found.</h2>
+      <p>We can't find the page you're looking for.</p>
+      <a href="/">Go back home</a>
+    </div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
-import logo from './assets/logo.png'
-import vector1 from './assets/vector_1.png'
-import vector2 from './assets/vector_2.png'
-import Clock from "./components/Clock"
-import coin1 from './assets/coin_1.png'
-import coin3 from './assets/coin_3.png'
-import { ToastContainer, toast } from 'react-toastify';
+
+import {  Route, Routes, BrowserRouter } from 'react-router-dom'
+import PageNotFound from './404';
+import Home from './Home';
 
 function App() {
-  let deadline = "November, 14, 2022";
 
   return (
-    <div className="main relative">
-      <ToastContainer />
-
-      <div className="main-secondary relative z-10">
-        <img className='absolute coin-one' src={coin1}/>
-        <img className='absolute coin-two' src={coin1}/>
-        <img className='absolute coin-three' src={coin3}/>
-        <img className='absolute coin-four' src={coin1}/>
-
-        <div className="grid grid-cols-1 place-items-center container h-auto">
-          <img className="w-32" src={logo} />
-          <img className='mt-32 ml-16 arch-img' src={vector1} />
-          <h1 className="text-3xl lg:text-6xl w-3/4 text-center leading-none font-semibold text-default-blue heading">
-            Begin your plan towards financial freedom
-          </h1>
-          <img className='mr-36 arch-img' src={vector2} />
-          <p className='text-xl mt-12 text-default-gray no-show'>Get notified when we launch by dropping your email adress</p>
-          <div className="grid mb-12 no-show">
-            <div>
-              <input className="mt-8 mr-4 email-box" type="text" name="email" placeholder="Enter you email address"/>
-            </div>
-            <div>
-              <button onClick={() => {}} className="mt-8 button-box">
-                Get Notified
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className='linear-container'>
-          <div className='grid grid-cols-1 justify-center'>
-            
-            <div className="mt-12 grid grid-cols-1 place-items-center mb-8">
-              <p className='text-xl text-white mb-4 live-in'>We will be live in:</p>
-
-              <Clock deadline={deadline} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
